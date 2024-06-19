4faf098e458696bdb4905fecef1eae7450e4861d
Mysticdrew <drewhaas@gmail.com>
2024-06-19 14:04:50 -0500

    sorted

---
+++ Update.bat
@@ -0,0 +1,3 @@
+java -jar .\tool\ForgeToolkit-1.1-all.jar update src/main/resources/assets/journeymap/lang/en_us.json src/main/resources/assets/journeymap/lang/*.json
+java -jar .\tool\ForgeToolkit-1.1-all.jar flatten src/main/resources/assets/journeymap/lang/*.json
+java -jar .\tool\ForgeToolkit-1.1-all.jar sort src/main/resources/assets/journeymap/lang/*.json
